44e024e30eee3048b8234e2c8015c2378884effe
Krzysztof Młynarczyk <11165215+kmlyn@users.noreply.github.com>
2021-03-24 04:13:58 +0100

    23.3. Added Cc to email

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -24,12 +24,19 @@ class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail() {
 //Given
-        Mail mail = new Mail("test@test.com", "Test", "Test Message");
+        Mail mail = Mail.builder()
+                .mailTo("test@test.com")
+                .subject("Test")
+                .message("Test Message")
+                .toCc("testcc@test.com")
+                .build();
+
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
+        mailMessage.setCc(mail.getToCc());
 
         //When
         simpleEmailService.send(mail);
